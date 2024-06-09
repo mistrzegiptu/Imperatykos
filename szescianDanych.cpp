e057284637d81cba3edbc78b0743c1591495ae26
Mistrz Egiptu <mistrzegiptu@gmail.com>
2024-06-09 23:15:44 +0200

    Finished kawa

--- szescianDanych.cpp
+++ szescianDanych.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 
 using namespace std;
-
+int det(int matrix[32][32], int size)
+{
+    int determinant = 0;
+    if(size == 2)
+        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+    else if(size == 3)
+        return matrix[0][0]*matrix[1][1]*matrix[2][2] +
+        matrix[0][1]*matrix[2][1]*matrix[2][0] +
+        matrix[1][0]*matrix[2][1]*matrix[0][2] -
+        matrix[0][2]*matrix[1][1]*matrix[2][0] -
+        matrix[1][0]*matrix[0][1]*matrix[2][2] -
+        matrix[0][0]*matrix[2][1]*matrix[1][2];
+    else
+    {
+        int temp[32][32];
+        for(int index = 0; index < size; index++)
+        {
+            int x = 0;
+            for (int i = 1; i < size; i++)
+            {
+                int y = 0;
+                for (int j = 0; j < size; j++)
+                {
+                    if(j == index)
+                        continue;
+                    temp[x][y] = matrix[i][j];
+                    y += 1;
+                }
+                x += 1;
+            }
+            if(index%2 == 0)
+                determinant += matrix[0][index] * det(temp, size-1);
+            else
+                determinant += matrix[0][index] * det(temp, size-1) * (-1);
+        }
+    }
+    return determinant;
+}
 int main()
 {
     int n = 0, result = 0;
@@ -29,24 +66,26 @@ int main()
                     dl = -1;
                 if(v >= (n/2))
                     dv = -1;
-                if(d >= (n/2))
+                if(p >= (n/2))
                     dp = -1;
 
                 int x = l, y = v, z = p;
 
-                for(int i = 0; i < h; i++)
+                for(int i = 0; i <= h; i++)
                 {
-                    for(int j = 0; j < w; j++)
+                    y = v;
+                    for(int j = 0; j <= w; j++)
                     {
-                        for(int k = 0; k < d; k++)
+                        z = p;
+                        for(int k = 0; k <= d; k++)
                         {
                             if(x >= 0 && x < n && y >= 0 && y < n && z >= 0 && z < n)
                                 result += tab[x][y][z];
-                            x += dp;
+                            z += dp;
                         }
                         y += dv;
                     }
-                    z += dl;
+                    x += dl;
                 }
                 cout<<result<<endl;
                 result = 0;
@@ -54,19 +93,15 @@ int main()
             }
             case 'T':
             {
-                /*for i in range(n):
-                    for j in range(n-i):
-                        for k in range(n-j-i):
-                            print("*", end = '')*/
                 int l = 0, v = 0, p = 0, e = 0, dl = 1, dv = 1, dp = 1;
 
                 cin >> l >> v >> p >> e;
 
-                if(l > (n/2))
+                if(l >= (n/2))
                     dl = -1;
-                if(v > (n/2))
+                if(v >= (n/2))
                     dv = -1;
-                if(p > (n/2))
+                if(p >= (n/2))
                     dp = -1;
 
                 int x = l, y = v, z = p;
@@ -113,20 +148,42 @@ int main()
                         {
                             if((x-l)*(x-l) + (y-v)*(y-v) + (z-p)*(z-p) <= r*r)
                                 result += tab[x][y][z];
-                            x += dp;
+                            z += dp;
                         }
                         y += dv;
                     }
-                    z += dl;
+                    x += dl;
                 }
                 cout<<result<<endl;
                 result = 0;
                 break;
             }
             case 'D':
-                break;
-            default:
+                char k = 0;
+                int index = 0;
+                cin>>k>>index;
+                int matrix[32][32];
+                for(int i = 0; i < n; i++)
+                {
+                    for(int j = 0; j < n; j++)
+                    {
+                        switch(k)
+                        {
+                            case 'l':
+                                matrix[i][j] = tab[index][i][j];
+                                break;
+                            case 'v':
+                                matrix[i][j] = tab[i][index][j];
+                                break;
+                            case 'p':
+                                matrix[i][j] = tab[i][j][index];
+                                break;
+                        }
+                    }
+                }
+                cout<<det(matrix, n)<<endl;
                 break;
         }
+        cin>>c;
     }
 }
